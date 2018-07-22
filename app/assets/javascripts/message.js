d4aa7884c2e435d76e2bf73b87e886db2fd1a55e
sixokrock7 <sixokrock.7@gmail.com>
2018-07-22 20:09:11 +0900

    Chat-space message.js done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,17 @@
 $(function() {
+
+  function buildHTML(message) {
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${message.user_id}></a>
+                    :
+                    </strong>
+                    ${message.content}
+                    :
+                    ${message.image}
+                  </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +24,10 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.textbox').val('')
+    })
   })
-})
+});
