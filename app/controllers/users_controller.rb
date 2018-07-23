a362643d07d0809c06efb9e3bd94ce390faf4f28
sixokrock7 <sixokrock.7@gmail.com>
2018-07-23 16:59:11 +0900

    Chat-space-message-comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    #もしログイン中のユーザーのテーブルの値が更新されたら
     if current_user.update(user_params)
+      #トップページへリダイレクト
       redirect_to root_path
     else
+      #editのビューを再描画
       render :edit
     end
   end
@@ -13,6 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
+    #userテーブルに制限して、nameカラムとemailカラムの値をパラメーターとして渡す
     params.require(:user).permit(:name, :email)
   end
 end
