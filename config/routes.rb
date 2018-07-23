8159a9aa96dcfbda69c8ee153490b432a0e9a7f5
sixokrock7 <sixokrock.7@gmail.com>
2018-07-23 18:12:16 +0900

    Chat-space-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #ルートパスにmessagesコントローラーのindexアクションを設定
   root "messages#index"
+  #usersモデルのindex,edit,updateアクションのルーティングを設定
   resources :users, only: [:index, :edit, :update]
+  #groupsモデルのnew,create,edit,updateアクションのルーティングを設定
   resources :groups, only: [:new, :create, :edit, :update] do
+    #messagesモデルのindex,createアクションのルーティングを設定
     resources :messages, only: [:index, :create]
   end
 end
